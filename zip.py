a5dbdf7918f2090ab10f2d6fb0fc2d20d3ba837f
Scottie Mai <scottmai@usc.edu>
2019-04-01 16:29:57 -0700

    v2

--- zip.py
+++ zip.py
@@ -9,13 +9,22 @@ def main():
 		line = line.strip('# \n')
 		lineArr = line.split()
 		if i == 0:
+			if len(lineArr) != 2:
+				print('Name line length error')
+				quit()
 			fname = lineArr[0]
 			lname = lineArr[1]
 		elif i == 2:
+			if len(lineArr) != 2:
+				print('Assignment line length error')
+				quit()
 			if lineArr[0].lower() == 'assignment':
 				filetype = 'a'
-			else:
+			elif lineArr[0].lower() == 'lab':
 				filetype = 'l'
+			else:
+				print('Assignment type error')
+				quit()
 			assignmentNum = lineArr[1]
 	ifile.close()
 
@@ -23,6 +32,9 @@ def main():
 	shutil.copy(file, newName + '.py')
 	ofile = zipfile.ZipFile(newName + '.zip', 'w')
 	ofile.write(newName + '.py')
+	for i in range(2, len(sys.argv)):
+		print(str(i))
+		ofile.write(sys.argv[i])
 	ofile.close()
 	os.remove(newName + '.py')
 main()
